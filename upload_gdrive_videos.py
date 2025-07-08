5b9662a075da242648cfacd1103debf7a797741f
Darkcoderd0 <abhijitden01@gmail.com>
2025-07-08 13:03:04 +0530

    Update upload_gdrive_videos.py

--- upload_gdrive_videos.py
+++ upload_gdrive_videos.py
@@ -122,8 +122,8 @@ def get_youtube_credentials(channel_id=None, channel_name=None):
     
     # Channel name to Drive file ID mapping
     CHANNEL_DRIVE_LINKS = {
-        "ExplainoByte": "https://drive.google.com/file/d/1Nqz0M9wzY6b3lheLyHeECBaa99R0cN6n/view?usp=drive_link",
-        "QuickFlick": "https://drive.google.com/file/d/1dBN1co7w65fuNIDyWKuEaHECx9ckMxSR/view?usp=drive_link"
+        "ExplainoByte": "https://drive.google.com/file/d/1cK8k-Svu86-ezDw_wGgvrTcn6GrcdAWa/view?usp=sharing",
+        "QuickFlick": "https://drive.google.com/file/d/1mZrZz1cAnGEivK6QDihBk4sGkr6J8-Ys/view?usp=sharing"
     }
     
     if not channel_name and not channel_id:
@@ -189,8 +189,8 @@ def list_available_youtube_channels():
     """List all channels that have saved YouTube tokens on Google Drive."""
     # Channel token Google Drive links
     CHANNEL_DRIVE_LINKS = {
-        "ExplainoByte": "https://drive.google.com/file/d/1Nqz0M9wzY6b3lheLyHeECBaa99R0cN6n/view?usp=drive_link",
-        "QuickFlick": "https://drive.google.com/file/d/1dBN1co7w65fuNIDyWKuEaHECx9ckMxSR/view?usp=drive_link"
+        "ExplainoByte": "https://drive.google.com/file/d/1cK8k-Svu86-ezDw_wGgvrTcn6GrcdAWa/view?usp=sharing",
+        "QuickFlick": "https://drive.google.com/file/d/1mZrZz1cAnGEivK6QDihBk4sGkr6J8-Ys/view?usp=sharing"
     }
     
     print("\nAvailable YouTube Channels:")
